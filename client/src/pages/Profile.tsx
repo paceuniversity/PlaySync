452cc03a3812baa9314dbc2efba38b138947c1ba
Sajal Shrestha <shresthasajal03@gmail.com>
2025-04-03 17:55:09 -0400

    Pages Init

---
+++ client/src/pages/Profile.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Profile = () => {
+  return (
+    <div
+      className="align-items-center justify-content-center vw-100 vh-100"
+      style={{ backgroundColor: '#223142' }}
+    >
+      <Header />
+      <h4 className="text-4xl text-white font-bold mt-3">Profile</h4>
+    </div>
+  );
+};
+
+export default Profile;
