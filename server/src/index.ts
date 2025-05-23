aa4650f0e9c2640247199b28e1fd748650074994
Sajal Shrestha <shresthasajal03@gmail.com>
2025-05-22 23:32:28 -0400

    front end public config

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://play-sync-sigma.vercel.app/'],
     credentials: true,
   })
 );
