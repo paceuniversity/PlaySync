f9d8b44d21285a55fb7b9063e1e104417327f755
Sajal Shrestha <shresthasajal03@gmail.com>
2025-05-22 22:07:26 -0400

    backend config

--- server/src/firebase/firebase.ts
+++ server/src/firebase/firebase.ts
@@ -1,8 +1,9 @@
 import admin from 'firebase-admin';
-import serviceAccount from './playsync-firebase-admin.json';
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG!);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
